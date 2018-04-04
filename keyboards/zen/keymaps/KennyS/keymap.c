c144eabf6b65d4a0fadf6492bf3178c6ea37803a
Kenny Sanders <kennysandersschool@gmail.com>
2018-04-04 19:19:15 -0400

    moded zen keymap

--- keyboards/zen/keymaps/KennyS/keymap.c
+++ keyboards/zen/keymaps/KennyS/keymap.c
@@ -15,6 +15,7 @@ extern keymap_config_t keymap_config;
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   NAV,
+  RGB,
 
 };
 
@@ -42,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                        KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSLS, \
   LT(_NAV, KC_CAPS),KC_A,    KC_S,    KC_D,    KC_F,     KC_G,                        KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,                        KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
-  KC_LCTL,          KC_LGUI, KC_LALT, ____,    ____,   KC_SPC, MO(_NAV), MO(_NAV), KC_BSPC, MO(_NAV), KC_MINS, KC_RALT, KC_RGUI, KC_RCTL \
+  KC_LCTL,          KC_LGUI, KC_LALT, KC_TRNS, MO(_RGB), KC_SPCE, MO(_NAV), MO(_NAV), KC_BSPC, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS \
 ),
 
 /* NAV
@@ -59,12 +60,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `------------------------------------------------''-----------------------------------------------'
  */
 [_NAV] = KEYMAP( \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7,   KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12 ,  \
+  KC_TRNS, KC_HOME, KC_UP  , KC_END , KC_PGUP, KC_LBRC,                      KC_RBRC, KC_TRNS,  KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS , \
+  KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_TRNS,                      KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS , \
+  KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNs, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS , \
+  KC_LCTL, KC_LGUI, KC_LALT, KC_TRNS, MO(_RGB), KC_SPCE, MO(_NAV), MO(_NAV), KC_BSPC, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS \
+),
+
+[_RGB] = KEYMAP(							\
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,             KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12 ,  \
   KC_TRNS, RGB_SAI, RGB_VAI, RGB_SAD, RESET,   KC_LBRC,                   KC_RBRC,           KC_PGUP,  KC_UP,   KC_PGDN, KC_INS,  KC_HOME , \
   KC_TRNS, RGB_HUD, RGB_VAD, RGB_HUI, KC_TRNS, KC_TRNS,                   KC_TRNS,           KC_LEFT,  KC_DOWN, KC_RGHT, KC_DEL,  KC_END , \
   KC_LSFT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   MAGIC_TOGGLE_NKRO, KC_TRNS,  KC_TRNS, KC_MPLY, KC_MPRV, KC_MNXT , \
-  KC_LCTL, KC_LGUI, KC_LALT, RGB_MOD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS, KC_MUTE, KC_VOLU, KC_VOLD \
+  KC_LCTL, KC_LGUI, KC_LALT, KC_TRNS, MO(_RGB),KC_SPCE,MO(_NAV),MO(_NAV), KC_BSPC,           KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS \
 ),
+
 
 };
 
